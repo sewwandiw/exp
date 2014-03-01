52fa4b7b6b8f9e66757b196c88d22abe37435d59
sewwandiw <sewwandi.wijayaratna@gmail.com>
2014-03-01 12:56:57 -0500

    deleted

--- c/scan_array.cpp
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-
-
-void scan_array(int numbers){
-	printf("%d",numbers);
-}
-
--- c/swap_no.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-
-int sawp_no(int a,int b){
-
-printf("a is %d \n  b is %d \n ",(a+b)-a,(a+b)-b);
-
-}
